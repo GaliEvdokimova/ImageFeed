3d07e833e70c3a13ed2fc898ca106b4e0867c1ed
GaliEvdokimova <galina.evdokimova5@icloud.com>
2024-06-20 00:02:10 +0300

    Констрейнты

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,12 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Galina evdokimova on 19.06.2024.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
